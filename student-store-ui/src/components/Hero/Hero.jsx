477dfbc0fce05b1ab30e64c6b161427b77ae28b6
Daniel Aguilar <danjagmeta@meta.com>
2023-06-22 00:09:40 -0700

    Navbar and Hero Styling Updates

--- student-store-ui/src/components/Hero/Hero.jsx
+++ student-store-ui/src/components/Hero/Hero.jsx
@@ -5,8 +5,8 @@ import HeroImage from "../../visuals/student_store_icon.18e5d61a.svg";
 export default function Navbar() {
     return (
       <div className="hero">
-        <div className="content">
-            <div className="intro">
+        <div className="hero-content">
+            <div className="hore-intro">
                 <h1>Welcome!</h1>
                 <h1>Find your Merch!</h1>
                 <p>We have all kinds of goodies. Click on any of the items to start filling up your shopping cart. Checkout whenever you're ready.</p>
--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -6,7 +6,8 @@ import { BsMeta } from "react-icons/bs";
 
 export default function Navbar() {
   return (
-    <nav className="navbar-content">
+    <nav className="navbar">
+      <div className="navbar-content">
         <Logo/>
         <div className="navbar-icons">
           <span><BsInstagram /></span>
@@ -18,6 +19,7 @@ export default function Navbar() {
           <span>About Us</span> 
           <span>Contact Us</span>
           <span>Buy Now</span>
+        </div>
         </div>
     </nav>
   )
