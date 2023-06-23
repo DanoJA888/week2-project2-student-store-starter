3b8bf4e64d2aceee23a3ad9cecd63b80134a503d
Daniel Aguilar <danjagmeta@meta.com>
2023-06-23 00:55:33 -0700

    Styled search component

--- student-store-ui/src/components/Search/Search.jsx
+++ student-store-ui/src/components/Search/Search.jsx
@@ -4,24 +4,27 @@ import "./Search.css"
 const categories = ["All Categories", "food", "clothing", "accessories", "tech"];
 export default function Search({filtering, currentSearch, setSearch}){
     return(
-        <div>
-            <div className="search-section"></div>
-                <input
-                    id = "inputField"
-                    type="text"
-                    value = {currentSearch}
-                    onChange={(e) => {setSearch(e.target.value)}}
+        <div className="search">
+            <div className="search-content">
+                <div className="search-section">
+                    <input
+                        id = "inputField"
+                        type="text"
+                        value = {currentSearch}
+                        onChange={(e) => {setSearch(e.target.value)}}
 
-                ></input>
-                <p>{currentSearch}</p>
-                <button>Search</button>
-            
-            <div className="filter-section">
-                <button onClick={() => {filtering(categories[0])} }>All Items</button>
-                <button onClick={() => {filtering(categories[1])}}>Food</button>
-                <button onClick={() => {filtering(categories[2])}}>Clothing</button>
-                <button onClick={() => {filtering(categories[3])}}>Accessories</button>
-                <button onClick={() => {filtering(categories[4])}}>Tech</button>
+                    ></input>
+                    <button>Search</button>
+                </div>
+                    
+                
+                <div className="filter-section">
+                    <button onClick={() => {filtering(categories[0])} }>All Items</button>
+                    <button onClick={() => {filtering(categories[1])}}>Food</button>
+                    <button onClick={() => {filtering(categories[2])}}>Clothing</button>
+                    <button onClick={() => {filtering(categories[3])}}>Accessories</button>
+                    <button onClick={() => {filtering(categories[4])}}>Tech</button>
+                </div>
             </div>
         </div>
     )
